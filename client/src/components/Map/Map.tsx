bdcddff5117ead847cb7ebafd87e495db957ce4e
AlexeyTrofimenko <byalexmail@gmail.com>
2024-05-05 19:39:40 +0300

    Add quests placements

--- client/src/components/Map/Map.tsx
+++ client/src/components/Map/Map.tsx
@@ -1,7 +1,11 @@
-import * as Location from 'expo-location';
-import React, { useEffect, useState } from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import React, { useState } from 'react';
+import { Button, Image, Modal, StyleSheet, Text, View } from 'react-native';
+import MapView, { Marker, Region } from 'react-native-maps';
+
+import { MarkerData } from '@/components/Map/MarkerData';
+import Menu from '@/components/Map/Menu';
+import { getDistance } from '@/components/Map/Utils';
+import useLocationTracker from '@/components/Map/useLocationTracker';
 
 const styles = StyleSheet.create({
   container: {
@@ -11,47 +15,94 @@ const styles = StyleSheet.create({
   mapContainer: {
     flex: 2
   },
-  menuContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'lightgray'
+  modalContainer: {
+    marginTop: 50,
+    marginHorizontal: 20,
+    padding: 20,
+    backgroundColor: 'white',
+    borderRadius: 20
   },
-  menuItem: {
-    flex: 1,
-    backgroundColor: 'darkgray',
-    alignItems: 'center',
-    justifyContent: 'center'
+  playerMarker: {
+    width: 50,
+    height: 50
+  },
+  questMarker: {
+    width: 45,
+    height: 50
   }
 });
 
 const Map = () => {
-  const [location, setLocation] = useState<Location.LocationObject | null>(
-    null
-  );
+  let mapView: MapView | null;
 
-  useEffect(() => {
-    (async () => {
-      const { status } = await Location.requestForegroundPermissionsAsync();
-      if (status === 'granted') {
-        Location.watchPositionAsync(
-          {
-            accuracy: Location.Accuracy.High,
-            timeInterval: 300,
-            distanceInterval: 2
-          },
-          (loc: Location.LocationObject) => {
-            setLocation(loc);
-          }
-        );
-      }
-    })();
-  }, []);
+  // Temporary sol
+  const markers: MarkerData[] = [
+    {
+      id: 1,
+      title: 'Потрібно купити їжи',
+      description: '1 кг картоплі, 3 кг рису',
+      category: 'Доставка',
+      reward: '400 QP',
+      latitude: 48.78825,
+      longitude: 34.4324,
+      creator: 'Антон П.',
+      created_at: '4 Травня 21.00'
+    },
+    {
+      id: 2,
+      title: 'Потрібна допомога у дворі',
+      description: 'Збери 10 ресурсів у дворі',
+      category: 'Збір',
+      reward: '100 QP',
+      latitude: 50.78825,
+      longitude: 30.4324,
+      creator: 'Володимир З.',
+      created_at: '3 Травня 08.00'
+    },
+    {
+      id: 3,
+      title: 'Вигуляй собаку',
+      description: 'Фото звіт з 3-х місць, і щоб не менше 1км відстані',
+      category: 'Вигул',
+      reward: '1000 QP',
+      latitude: 48.4801,
+      longitude: 34.0212,
+      creator: 'Жанна Д.',
+      created_at: '3 Травня 16.00'
+    }
+  ];
+
+  const location = useLocationTracker();
+  const [selectedMarker, setSelectedMarker] = useState<MarkerData | null>(null);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [showDetailsButton, setShowDetailsButton] = useState<number | null>();
+
+  const maxDistance = 1000;
+
+  const onRegionChangeComplete = (region: Region) => {
+    if (
+      location &&
+      getDistance(
+        region.latitude,
+        region.longitude,
+        location.coords.latitude,
+        location.coords.longitude
+      ) > 3000
+    ) {
+      mapView?.animateToRegion({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005
+      });
+    }
+  };
 
   return (
     <View style={styles.container}>
       <MapView
         style={styles.mapContainer}
-        region={
+        initialRegion={
           location
             ? {
                 latitude: location.coords.latitude,
@@ -61,6 +112,16 @@ const Map = () => {
               }
             : undefined
         }
+        minZoomLevel={14}
+        maxZoomLevel={18}
+        ref={(ref) => {
+          mapView = ref;
+        }}
+        onRegionChangeComplete={onRegionChangeComplete}
+        onPress={() => {
+          setSelectedMarker(null);
+          setShowDetailsButton(null);
+        }}
       >
         {location && (
           <Marker
@@ -72,26 +133,96 @@ const Map = () => {
               location.coords.heading ? location.coords.heading : undefined
             }
             anchor={{ x: 0.5, y: 0.5 }}
-            title="It's you. Try start looking for QuestPoints"
+            title="Це ти. Спробуй знайти квести поблизу."
+            onPress={() => {
+              setSelectedMarker(null);
+              setShowDetailsButton(null);
+            }}
           >
             <Image
               source={require('assets/player_marker.webp')}
-              style={{ width: 50, height: 50 }}
+              style={styles.playerMarker}
             />
           </Marker>
         )}
+        {location &&
+          markers
+            .filter(
+              (marker) =>
+                getDistance(
+                  location.coords.latitude,
+                  location.coords.longitude,
+                  marker.latitude,
+                  marker.longitude
+                ) <= maxDistance
+            )
+            .map((marker) => (
+              <Marker
+                key={marker.id}
+                coordinate={{
+                  latitude: marker.latitude,
+                  longitude: marker.longitude
+                }}
+                title={marker.title}
+                description={marker.description}
+                onPress={() => {
+                  setSelectedMarker(marker);
+                  setShowDetailsButton(marker.id);
+                }}
+              >
+                <Image
+                  source={require('assets/quest_marker.png')}
+                  style={styles.questMarker}
+                />
+              </Marker>
+            ))}
       </MapView>
-      <View style={styles.menuContainer}>
-        <View style={styles.menuItem}>
-          <Text>Q</Text>
-        </View>
-        <View style={styles.menuItem}>
-          <Text>M</Text>
-        </View>
-        <View style={styles.menuItem}>
-          <Text>P</Text>
-        </View>
+      <View>
+        {selectedMarker && showDetailsButton === selectedMarker.id && (
+          <Button
+            title="Детальніше"
+            onPress={() => {
+              setModalVisible(true);
+              setShowDetailsButton(null);
+            }}
+          />
+        )}
       </View>
+      <Modal
+        animationType="slide"
+        transparent
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.modalContainer}>
+          {selectedMarker && (
+            <>
+              <Text>{selectedMarker.title}</Text>
+              <Text>Опис: {selectedMarker.description}</Text>
+              <Text>Нагорода: {selectedMarker.reward} QP</Text>
+              <Text>
+                Відстань:
+                {location
+                  ? `${getDistance(location.coords.latitude, location.coords.longitude, selectedMarker.latitude, selectedMarker.longitude).toFixed(2)} кроків`
+                  : 'Розраховую...'}
+              </Text>
+              <Text>Замовник: {selectedMarker.creator}</Text>
+              <Text>{selectedMarker.created_at}</Text>
+              <Button
+                onPress={() => {
+                  // TODO Accept quest
+                  console.log('Прийняти квест!');
+                  setModalVisible(false);
+                }}
+                title="Хочу виконати"
+              />
+            </>
+          )}
+        </View>
+      </Modal>
+      <Menu />
     </View>
   );
 };
