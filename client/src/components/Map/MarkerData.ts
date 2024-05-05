1640ae2a72c306c1a4a0af46fc26cc56a8e4a98b
AlexeyTrofimenko <byalexmail@gmail.com>
2024-05-05 19:38:20 +0300

    Add marker interface

---
+++ client/src/components/Map/MarkerData.ts
@@ -0,0 +1,11 @@
+export interface MarkerData {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  reward: string;
+  latitude: number;
+  longitude: number;
+  creator: string;
+  created_at: string;
+}
